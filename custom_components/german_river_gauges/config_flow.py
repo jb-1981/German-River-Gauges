b522cf38cc70f733d5dfb68c9e658725704830a1
jb-1981 <j81@posteo.de>
2026-07-01 11:24:53 +0200

    Add config flow for German River Gauges
    Implement configuration flow for German River Gauges integration.

---
+++ custom_components/german_river_gauges/config_flow.py
@@ -0,0 +1,32 @@
+import voluptuous as vol
+from homeassistant import config_entries
+
+from .const import DOMAIN, RIVERS, CONF_RIVERS
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for German River Gauges."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """First step of config flow."""
+        if user_input is not None:
+            return self.async_create_entry(
+                title="German River Gauges",
+                data=user_input,
+            )
+
+        schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_RIVERS,
+                    default=list(RIVERS.keys()),
+                ): list,
+            }
+        )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+        )
