2f40303bd32fee21d69a90b7b67ef31ec0b66f04
jb-1981 <j81@posteo.de>
2026-07-01 11:35:46 +0200

    Refactor setup functions to use ConfigEntry

--- custom_components/german_river_gauges/__init__.py
+++ custom_components/german_river_gauges/__init__.py
@@ -1,14 +1,31 @@
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .api import PegelOnlineAPI
+from .coordinator import RiverDataCoordinator
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up via YAML is not supported."""
+    """Set up via YAML (not used)."""
     return True
 
 
-async def async_setup_entry(hass: HomeAssistant, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up integration from a config entry."""
+
+    api = PegelOnlineAPI()
+    coordinator = RiverDataCoordinator(hass, api)
+
+    await coordinator.async_config_entry_first_refresh()
+
     hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload integration."""
+    hass.data[DOMAIN].pop(entry.entry_id, None)
     return True
